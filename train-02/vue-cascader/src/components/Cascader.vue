cc31341ec3aff2e113a747f0879257f599290254
ornedk <ornedk@qq.com>
2019-08-22 22:24:45 +0800

    2.3自定义全局指令:实现点击输入框以外的内容应该收起面板

--- train-02/vue-cascader/src/components/Cascader.vue
+++ train-02/vue-cascader/src/components/Cascader.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    <div class="trigger" @click="isVisible=!isVisible">
+  <!-- 1.需求：点击输入框以外的内容应该收起面板   ->>> 将功能扩展为指令 -->
+  <div v-click-outside="close">
+    <div class="trigger" @click="toggle">
       <slot></slot>
     </div>
     <div class="content" v-if="isVisible">显示面板</div>
@@ -14,6 +15,34 @@ export default {
     return {
       isVisible: false
     };
+  },
+  //   指令封装
+  directives: {
+    clickOutside: {
+      //inserted是指令当中的钩子函数(生命周期:被绑定元素插入父节点时调用 (仅保证父节点存在，但不一定已被插入文档中)。)
+      inserted(el, bindings) {
+        //   addEventListener:将指定的监听器注册到对象上，当该对象触发指定的事件时，指定的回调函数就会被执行。
+        document.addEventListener("click", e => {
+          // 如果
+          console.log(e, bindings);
+          // 如果传参的target属性等于el 或者el.contains(e.target))为true(点击的不是trigger和content返回true)
+          if (e.target === el || el.contains(e.target)) {
+            //   直接返回
+            return;
+          }
+          // 点击非自己、或者不是自己的儿子就关闭元素(vlue中的参数为close,也就是this.isVisible = false)
+          bindings.value();
+        });
+      }
+    }
+  },
+  methods: {
+    close() {
+      this.isVisible = false;
+    },
+    toggle() {
+      this.isVisible = !this.isVisible;
+    }
   }
 };
 </script>
