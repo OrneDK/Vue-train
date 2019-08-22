aeea7a69cc44668f0b5dd339c0a0f87fb8717fdb
ornedk <ornedk@qq.com>
2019-08-22 21:47:17 +0800

    2.2:实现组件的显示隐藏切换

--- train-02/vue-cascader/src/components/Cascader.vue
+++ train-02/vue-cascader/src/components/Cascader.vue
@@ -1,11 +1,27 @@
 <template>
-    <div>
-        Cascader
+  <div>
+    <div class="trigger" @click="isVisible=!isVisible">
+      <slot></slot>
     </div>
+    <div class="content" v-if="isVisible">显示面板</div>
+  </div>
 </template>
 
 <script>
 export default {
-    name:'Cascader'
-}
+  name: "Cascader",
+  data() {
+    return {
+      isVisible: false
+    };
+  }
+};
 </script>
+
+<style lang="stylus" scoped>
+.trigger {
+  width: 150px;
+  height: 25px;
+  border: 1px solid #cc;
+}
+</style>
